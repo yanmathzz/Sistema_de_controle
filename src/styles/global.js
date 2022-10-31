fb92ed577153224fac4a3e482a4852fc943346e1
Yan <ymatheus706@gmail.com>
2022-10-31 00:51:18 -0300

    publicaçao

---
+++ src/styles/global.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+  
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: 'Poppins', Sans-Serif;
+    background-color: #D4D2D4;
+  }
+`;
+
+export default Global;
